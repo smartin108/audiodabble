891773319190c7ae88a60c4e930136e07af71e20
Andy Holaday <smartin108@gmail.com>
2024-07-03 21:48:21 -0400

    a decent working version

--- moar_data2wav.py
+++ moar_data2wav.py
@@ -1,22 +1,46 @@
 import numpy as np
 from scipy.io.wavfile import write
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from pathlib import Path
 
 
+def dB2dec(dB):
+    return 10**(dB/20)
 
 
-def get_file_content(col=1):
-    data = np.genfromtxt(myfile, delimiter=',', skip_header=1)
-    return data[:, col]
+def dec2dB(dec):
+    return 20 * np.log10(dec)
 
+
+def get_file_content(filename:str, col=1, norm=-3):
+    data = np.genfromtxt(filename, delimiter=',', skip_header=1)
+    if norm:
+        max_value = np.abs(data[:, col]).max() # normalize per-channel
+        # max_value = np.abs(data[:, 1:4]).max() # normalize per-data set
+    adjustment_factor = dB2dec(norm) / max_value
+    print(max_value, dB2dec(norm), adjustment_factor)
+    return data[:, col] * adjustment_factor
+
+
+def get_parent_folder_name(fqpn:str):
+    return Path(fqpn).parent.name
+
+
+bit_depth = 16
 sample_rate = 44100
-amplitude = 32767
+amplitude = 2**(bit_depth-1) - 1
+# amplitude = 32767
 counter = 1
 
-for file in range(1,3):
-    for col in range(1,4):
-        myfile = f'Raw Data {file}.csv'
-        data = get_file_content(col=col)
-        data = amplitude * data
-        data = data.astype(np.int16)
-        write(f'output {counter}.wav', sample_rate, data)
-        counter += 1
+Tk().withdraw()
+filename = askopenfilename()
+experiment_name = get_parent_folder_name(filename)
+
+for col in range(1,4):
+    data = get_file_content(filename=filename, col=col)
+    data = amplitude * data
+    data = data.astype(np.int16)
+    outfile_name = f'{experiment_name}.csv'
+    write(f'{experiment_name}-{counter}.wav', sample_rate, data)
+    counter += 1
